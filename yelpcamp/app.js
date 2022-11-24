2653bc07a1b1dff2e186c7285724d9233e596418
dev_josh <Alanajoshua535@gmail.com>
2022-11-24 23:56:11 +0100

    files routes

---
+++ yelpcamp/app.js
@@ -0,0 +1,36 @@
+const express  = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campgrounds')
+
+mongoose.connect('mongodb+srv://Josh-Dev:Alana_2001@joshua.wbl0vjk.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+ 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.get('/', (req, res) => {
+res.render('home')
+})
+
+app.get('/campgrounds', async (req, res) => {
+const camp = new Campground({title: "youuuo"});
+await camp.save();
+res.send(camp);
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
