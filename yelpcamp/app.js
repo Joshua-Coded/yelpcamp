59c6d44d71a8c94431e6c6fa49dd349471a4b07a
dev_josh <Alanajoshua535@gmail.com>
2022-11-26 15:07:46 +0100

    phase one

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -20,6 +20,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+app.use(express.urlencoded({extended: true}));
+
+
 
 app.get('/', (req, res) => {
 res.render('home')
@@ -30,11 +33,22 @@ const campgrounds =  await Campground.find({})
 res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res) => {   
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 
-})
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
